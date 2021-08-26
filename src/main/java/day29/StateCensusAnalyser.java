589ceaac4d3bf1fdf336c90d807f68723a3e9561
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-26 21:28:40 +0530

    first commit

---
+++ src/main/java/day29/StateCensusAnalyser.java
@@ -0,0 +1,15 @@
+package day29;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+
+import com.opencsv.CSVReader;
+
+public class StateCensusAnalyser {
+
+	static String CVSfile = "src\\main\\resources\\CSVfile.csv";
+	static int count = 0;
+
+
+}
